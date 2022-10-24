1c90aa842536a13cb19191a1f0bc1af437a335e5
Diego Fernandes <superdiegao@gmail.com>
2022-10-24 17:53:40 -0300

    versao final

--- src/viking.js
+++ src/viking.js
@@ -17,8 +17,6 @@ class Soldier {
 
 let qqsoldado = new Soldier(100,50)
 
-
-
 // Viking
 class Viking extends Soldier { 
     constructor (name, health, strength){
@@ -48,5 +46,51 @@ class Saxon extends Soldier {
     }   
 }
 
-// War
-class War { }
+// War exercício mais difícil. Tentei qubrar a cabeça ao máximo, mas precisei de ajuda para resolver
+class War {
+    constructor () {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking (viking) {
+            this.vikingArmy.push(viking);
+    }
+    addSaxon (saxon) {
+        this.saxonArmy.push(saxon);
+    }
+    vikingAttack () {
+        
+        let vikingNum = Math.floor(Math.random()*this.vikingArmy.length);
+        let saxonNum = Math.floor(Math.random()*this.saxonArmy.length);
+
+        if (this.vikingArmy[vikingNum].attack()) {
+            const saxonDamage = this.saxonArmy[saxonNum].receiveDamage(this.vikingArmy[vikingNum].strength);
+            if (saxonDamage.includes('died')) { // === `A Saxon has died in combat`) 
+                this.saxonArmy.splice(saxonNum, 1);
+            }
+            return saxonDamage;
+          }
+    }
+    saxonAttack () {
+        let vikingNum = Math.floor(Math.random()*this.vikingArmy.length);
+        let saxonNum = Math.floor(Math.random()*this.saxonArmy.length);
+        
+        if (this.saxonArmy[saxonNum].attack()) {
+            const vikingDamage = this.vikingArmy[vikingNum].receiveDamage(this.saxonArmy[saxonNum].strength);
+            if (vikingDamage.includes('died')) {
+                this.vikingArmy.splice(vikingNum, 1);
+            }
+            return vikingDamage;
+          }
+    }
+    showStatus () {
+        if (this.saxonArmy.length == 0) {
+            return "Vikings have won the war of the century!";
+        } else if (this.vikingArmy.length == 0) {
+            return "Saxons have fought for their lives and survived another day...";
+        } else {
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
+
+    }
+}
