8a0c4dd85f433bd28b166abfd47b77a714f73b4e
Diego Fernandes <superdiegao@gmail.com>
2022-10-24 15:02:48 -0300

    itaracao 0 a 3

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,52 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack(){
+        return this.strength
+    }
+    receiveDamage (damage) {
+        this.damage = damage;
+        this.health = this.health - this.damage
+        
+    }
+}
+
+let qqsoldado = new Soldier(100,50)
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier { 
+    constructor (name, health, strength){
+        super( health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage
+        if(this.health <= 0) {return `${this.name} has died in act of combat`}    
+        return `${this.name} has received ${damage} points of damage`
+    }   
+
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage){
+        this.health = this.health - damage
+        if(this.health > 0) {return `A Saxon has received ${damage} points of damage`}    
+        if(this.health <= 0) {return `A Saxon has died in combat`}    
+    }   
+}
 
 // War
-class War {}
+class War { }
